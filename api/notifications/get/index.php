0f834ba8a830ae9ed054195c446fbbd8f9e5c957
Pablo Sanabria <pcsanabria@hotmail.com>
2020-06-27 10:57:04 -0300

    cambios

--- api/notifications/get/index.php
+++ api/notifications/get/index.php
@@ -11,7 +11,7 @@ global $access_token,$collector_id,$notificationJSON;
 
 $lastResource = file_get_contents('../notifications.txt');
 
-$url= $lastResource."&access_token=$access_token";
+$url= $lastResource."?access_token=$access_token";
 
 // REVISAR AQUÍ:
 // Agrega la $url necesaria para revisar el estado del pago en base al recurso recibido de la notificación
